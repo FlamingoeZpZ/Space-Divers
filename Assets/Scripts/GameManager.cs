3184c3a8e34b51379bc63a2eeb551ed7d33554d3
FlamingoeZpZ <52292616+FlamingoeZpZ@users.noreply.github.com>
2023-04-04 11:29:27 -0400

    Toggle material garbage M to init, K to toggle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [DefaultExecutionOrder(-100)]
 public class GameManager : MonoBehaviour
 {
+    
     
     public static GameManager instance { get; private set; }
     [field: SerializeField]   public Transform bulletParent { get; private set; }
@@ -24,10 +28,50 @@ public class GameManager : MonoBehaviour
         
         instance = this;
         DontDestroyOnLoad(instance);
+        
+
     }
 
     public void TEMP_END_GAME(bool win)
     {
         Instantiate(win?TEMP_WIN_OBJECT:TEMP_LOSS_OBJECT);
+    }
+
+    private List<MeshRenderer> mr = new();
+    private List<Material> saved = new();
+    private bool toggled;
+    [SerializeField] private LayerMask removeLayer;
+    private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            mr = FindObjectsByType<MeshRenderer>(FindObjectsInactive.Include, FindObjectsSortMode.None).ToList();
+            saved.Clear();
+            foreach (MeshRenderer m in mr)
+            {
+                saved.Add(m.material);
+            }
+            
+        }
+
+        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+
+
+            for (int i = 0; i < mr.Count; ++i)
+            {
+                if (((1 << mr[i].gameObject.layer) & removeLayer) == 0)
+                {
+                    continue;
+                }
+
+                mr[i].material = toggled ? saved[i] : null;
+            }
+
+
+
+            toggled = !toggled;
+        }
     }
 }
