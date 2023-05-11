407ab85a5c1dca45a9fba65d45c6527463dc1c3c
FlamingoeZpZ <gabekotton@gmail.com>
2023-05-11 19:09:15 -0400

    FileStructureFixes

---
+++ Assets/Scripts/Game/Enemy.cs
@@ -0,0 +1,105 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Enemy : BaseCharacter
+{
+    public static readonly Dictionary<uint, Blip> Blips = new();
+    private static uint _shipID;
+    private uint id;
+    private Vector3 target;
+    public static readonly Dictionary<uint, bool> Targeting = new();
+    
+    protected void Start()
+    {
+        curSpeed = stats.maxSpeed * 0.8f;
+        do
+        {
+            id = _shipID++;
+        } while (Blips.ContainsKey(id));
+        Blips.Add(id, new Blip(transform, stats.blip, id));
+        target = Random.insideUnitSphere * 100;
+        Targeting.Add(id, false);
+        
+        
+        
+        
+    }
+
+
+    private float huntTime;
+    protected override void Move()
+    {
+        
+        Vector3 v = (target - transform.position); // 
+        float mag = v.sqrMagnitude;
+
+        if (curTarget)
+        {
+            target = curTarget.position;
+            Targeting[id] = true;
+        }
+
+        if (mag < 4)
+        {
+            v = Random.insideUnitSphere;
+            v.y *= 0.7f;
+            target = v * 100;
+        }
+        
+        //This may be stupid, but if we can get it as a vector 3, then we can conform to the rules.
+        Quaternion toRotation = Quaternion.LookRotation(v);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, stats.baseHandling * Time.deltaTime); //This is a cheat, but I'm struggling to get things to work w/ euler
+        
+        base.Move();
+
+        Debug.DrawLine(transform.position, target, Color.green);
+
+        
+    }
+
+    public override void UpdateHealth(Transform attacker, float damage)
+    {
+        base.UpdateHealth(attacker, damage);
+
+        print("I've been hit!" + damage +" by: " + attacker );
+        if (damage < 0)
+        {
+            curTarget = attacker;
+            Targeting[id] = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        print("Destroyed: " + id);
+        //Convert this to array pool.
+        Blips[id].DestroyBlip();
+        Blips.Remove(id);
+        Targeting.Remove(id);
+        CustomPool.Instance.Explode(transform.position);
+        #if UNITY_EDITOR
+                if (Application.isEditor && !Application.isFocused)
+                    return;
+        #endif
+        
+        /* TODO fix
+        
+        if (Blips.Count == 0)
+        {
+            GameManager.instance.TEMP_END_GAME(true);
+        }*/
+    }
+
+    public override void OnTargeted()
+    {
+        //Apply materials.
+        //Record time, and perform evaisive manuevers 
+    }
+
+    public override void OnUnTargeted()
+    {
+        //Remove materials
+        //Reset time
+    }
+}
---
+++ Assets/Scripts/Game/Turret.cs
@@ -0,0 +1,76 @@
+using System;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    private int targetLayers;
+    [SerializeField] private LayerMask otherLayers;
+    [SerializeField] private int maxWeapons;
+    [SerializeField] private float swivelSpeed;
+    [SerializeField, Range(0, 1)] private float angBeforeShooting = 0.04f;
+    [SerializeField, Range(0, 1)] private float maxAng = 0.1f;
+    private readonly Collider [] hits =new Collider[1];
+    private Weapon[] weapons;
+    private int id;
+    private Transform root;
+    private void Awake()
+    {
+        targetLayers = 1<<LayerMask.NameToLayer("Enemy");
+        weapons = new Weapon[maxWeapons];
+        root = transform.root.GetChild(0);
+    }
+
+    public void AppendWeapon(Weapon w)
+    {
+        weapons[id++] = w;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Scan for targets
+        //bool h = Physics.SphereCast(transform.position, 50, transform.forward, out hit, 50,d);
+        int h = Physics.OverlapSphereNonAlloc(transform.position, 250, hits,  targetLayers);
+        if (id == 0 || h == 0) return;
+        
+        //If able to look at target
+        Vector3 forward = transform.forward;
+        //If we have LOS
+        Vector3 lookVec = hits[0].bounds.center - transform.position; // Look in direction
+        Debug.DrawRay(transform.position, lookVec, Color.yellow);
+        if (Physics.Raycast(transform.position, lookVec, out RaycastHit t,lookVec.magnitude,  otherLayers))// Cringe as hell.
+        {
+            Debug.DrawRay(transform.position - forward, forward * 50, Color.red);
+            return;
+        }
+        
+        Debug.DrawRay(transform.position - forward, forward * 50, Color.green);
+        
+        
+
+        Quaternion preRot = transform.rotation;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookVec), Time.deltaTime * swivelSpeed);
+
+        //If Exceeding max rotation, don't allow rotation
+        float dot = Vector3.Dot(forward, root.up);
+        //If we are currently out of bounds AND we are trying to go further out of bounds
+        if (Mathf.Abs(dot) > maxAng && Mathf.Abs(dot) < Mathf.Abs(Vector3.Dot(transform.forward, root.up)))
+        {
+            transform.rotation = preRot;
+        }
+
+        
+        if (Vector3.Dot(transform.forward, lookVec) > angBeforeShooting)
+        {
+            for (int i = 0; i < id; ++i)
+            {
+                weapons[i].TryShoot(hits[0].transform);
+            }
+        }
+        //
+
+
+    }
+}
